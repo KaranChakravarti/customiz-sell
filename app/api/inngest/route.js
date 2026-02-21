511308f4361f014971038638f4a25783bdfe59f9
KaranChakravarti <kk29631555@gmail.com>
2026-02-21 16:55:54 +0530

    uploading initial file again on github

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,12 +1,12 @@
-import {serve} from "inngest/next";
-import { inngest, syncUserCreation, syncUserDeletion, synUserUpdation } from "@/config/inngest";
+// import {serve} from "inngest/next";
+// import { inngest, syncUserCreation, syncUserDeletion, synUserUpdation } from "@/config/inngest";
 
-// create an api that serve zero function
-export const { GET, POST, put, } = serve({
-    client: inngest,
-    functions: [
-        syncUserCreation,
-        synUserUpdation,
-        syncUserDeletion
-    ],
-})
+// // create an api that serve zero function
+// export const { GET, POST, put, } = serve({
+//     client: inngest,
+//     functions: [
+//         syncUserCreation,
+//         synUserUpdation,
+//         syncUserDeletion
+//     ],
+// })
